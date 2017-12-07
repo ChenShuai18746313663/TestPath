9910a96d5e02dfe16d7863aa3a5c841bc5e5dab2
ChenShuai18746313663 <18746313663@163.com>
2017-12-07 08:41:28 +0800

    Initial commit

---
+++ Assets/DebPath.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class DebPath : MonoBehaviour {
+
+	public Text text;
+	public Text text1;
+
+	// Use this for initialization
+	void Start () {
+
+		StreamWriter sw = new StreamWriter (Application.streamingAssetsPath + "/Lua.txt");
+
+		try {
+			sw.Write("223445");;
+		} catch (System.Exception ex) {
+			text1.text = ex.ToString ();
+		}
+		sw.Close ();
+
+
+		StreamReader sr = new StreamReader (Application.streamingAssetsPath + "/Lua.txt", System.Text.Encoding.UTF8);
+		string str = "";
+		try {
+			str = sr.ReadToEnd();
+		} catch (System.Exception ex) {
+			text1.text = ex.ToString ();
+		}
+		sr.Close ();
+
+
+		#if UNITY_EDITOR
+		text.text = Application.streamingAssetsPath;
+		text1.text = Application.persistentDataPath;
+		#elif UNITY_STANDALONE_WIN
+		text.text = Application.streamingAssetsPath;
+		text1.text = Application.persistentDataPath;
+		#endif
+
+
+		text.text =str;
+		text1.text = Application.persistentDataPath;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
